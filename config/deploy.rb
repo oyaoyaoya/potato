5919de96c065019130a645ad7a7fbf6051413c31
Hayato oyanagi <oyana@oya.local>
2017-04-17 14:21:48 +0900

    add code

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,6 +4,8 @@ lock "3.8.0"
 set :application, "potato"
 set :repo_url, "git@github.com:oyaoyaoya/potato.git"
 
+set :ssh_options, :port => "443"
+
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
