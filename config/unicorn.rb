e2b60e2e39afc45b01c1bc752b9c1380cb5ec59d
Hayato oyanagi <oyana@oya.local>
2017-04-23 19:35:43 +0900

    ad unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,43 +1,41 @@
-# app_path = '/var/www/potato'
-#
-# worker_processes 1
+#ワーカーの数。後述
+  $worker  = 2
+#何秒経過すればワーカーを削除するのかを決める
+  $timeout = 30
+#自分のアプリケーション名、currentがつくことに注意。
+  $app_dir = "/var/www/potato/current"
+#リクエストを受け取るポート番号を指定。後述
+  $listen  = File.expand_path 'tmp/sockets/.unicorn.sock', $app_dir
+#PIDの管理ファイルディレクトリ
+  $pid     = File.expand_path 'tmp/pids/unicorn.pid', $app_dir
+#エラーログを吐き出すファイルのディレクトリ
+  $std_log = File.expand_path 'log/unicorn.log', $app_dir
 
-# working_directory "#{app_path}/current"
-# pid "#{app_path}/shared/tmp/pids/unicorn.pid"
-# stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
-# stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
+# 上記で設定したものが適応されるよう定義
+  worker_processes  $worker
+  working_directory $app_dir
+  stderr_path $std_log
+  stdout_path $std_log
+  timeout $timeout
+  listen  $listen
+  pid $pid
 
-# listen 3000
-# timeout 100
+#ホットデプロイをするかしないかを設定
+  preload_app true
 
-$worker  = 2
-$timeout = 30
-$app_dir = "/var/www/potato/current"
-$listen  = File.expand_path 'tmp/sockets/.unicorn.sock', $app_dir
-$pid     = File.expand_path 'tmp/pids/unicorn.pid', $app_dir
-$std_log = File.expand_path 'log/unicorn.log', $app_dir
-
-worker_processes  $worker
-working_directory $app_dir
-stderr_path $std_log
-stdout_path $std_log
-timeout $timeout
-listen  $listen
-pid $pid
-
-preload_app true
-
-before_fork do |server, worker|
-  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
-  old_pid = "#{server.config[:pid]}.oldbin"
-  if old_pid != server.pid
-    begin
-      Process.kill "QUIT", File.read(old_pid).to_i
-    rescue Errno::ENOENT, Errno::ESRCH
+#fork前に行うことを定義。後述
+  before_fork do |server, worker|
+    defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+    old_pid = "#{server.config[:pid]}.oldbin"
+    if old_pid != server.pid
+      begin
+        Process.kill "QUIT", File.read(old_pid).to_i
+      rescue Errno::ENOENT, Errno::ESRCH
+      end
     end
   end
-end
 
-after_fork do |server, worker|
-  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
-end
+#fork後に行うことを定義。後述
+  after_fork do |server, worker|
+    defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+  end
