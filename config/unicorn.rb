190a22803e9a6f59aa8bb123a348e6340539ea40
Hayato oyanagi <oyana@oya.local>
2017-04-24 13:37:45 +0900

    edit unicorn config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -30,7 +30,7 @@ preload_app true
 before_fork do |server, worker|
   defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
   old_pid = "#{server.config[:pid]}.oldbin"
-  if old_pid != server.pid
+  if File.exist?(old_pid) && server.pid != old_pid
     begin
       Process.kill "QUIT", File.read(old_pid).to_i
     rescue Errno::ENOENT, Errno::ESRCH
