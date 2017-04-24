4e7f00028b66709157d0569ec04216f518b0a96d
Hayato oyanagi <oyana@oya.local>
2017-04-24 13:00:32 +0900

    edit config/unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -14,7 +14,7 @@ $worker  = 2
 $timeout = 30
 $app_dir = "/var/www/potato/current"
 $listen  = File.expand_path 'tmp/sockets/.unicorn.sock', $app_dir
-$pid     = File.expand_path 'tmp/pids/unicorn.pid', $app_dir
+$pid     = File.expand_path '/var/www/potato/shared/tmp/pids/unicorn.pid'
 $std_log = File.expand_path 'log/unicorn.log', $app_dir
 
 worker_processes  $worker
