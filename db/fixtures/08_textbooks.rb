fb3a0d9e3827efc475fd97cac3cbf6d250b9f092
Hayato oyanagi <oyana@oya.local>
2017-05-05 20:05:29 +0900

    add seed_fu

--- db/fixtures/08_textbooks.rb
+++ db/fixtures/08_textbooks.rb
@@ -1,30 +1,20 @@
-require 'open-uri'
-# Nokogiriライブラリの読み込み
-require 'nokogiri'
 require "csv"
 
-# スクレイピング先のURL
-
-files = Dir.glob("db/csv/links/*")
-files.each do |file|
-  course_csv = CSV.read(file)
-  course_csv.each.with_index do |row|
-    url = row[0]
-    charset = nil
-    html = open(url) do |f|
-      charset = f.charset # 文字種別を取得
-      f.read # htmlを読み込んで変数htmlに渡す
+# 学部
+textbook_csv = CSV.read('db/csv/completed/textbook.csv')
+first_line = textbook_csv[0]
+colum_num = first_line.count
+textbook_csv.each.with_index do |row, i|
+  unless i == 0
+    s = Textbook.new do |s|
+      colum_num.times do |i|
+        s[ first_line[i] ] = row[i]
+      end
     end
-
-    # htmlをパース(解析)してオブジェクトを生成
-    doc = Nokogiri::HTML.parse(html, nil, charset)
-
-    d = doc.xpath('//div[3]')
-    e = d.xpath('//tr[11]')
-
-
-    f = doc.xpath('//*[@id="form"]/div[3]/table/tr[11]/td[3]')
-    d = f.inner_text
-    puts d.strip
+    if s.save
+      puts "save"
+    else
+      puts "failed"
+    end
   end
 end
