e5c886a19483b6d7fdd5e715b7616c999ad124e1
Hayato oyanagi <oyana@oya.local>
2017-09-22 12:14:39 +0900

    df

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -2,15 +2,15 @@ module UserDecorator
   def show_icon(id)
     case id
     when "0"
-      image_tag "icon.jpg", size: "20×20"
+      image_tag("icon.jpg", :size => "20×20", class:"circle")
     when "1"
-      image_tag "hoshimo.png", size: "20×20"
+      image_tag("hoshimo.png", :size => "20×20", class:"circle")
     when "2"
-      image_tag("dansyaku.jpg", :size => "20×20", class:"circle")
+      image_tag("satu.jpg", :size => "20×20", class:"circle")
     when "3"
-      image_tag "dansyaku.jpg", size: "20×20"
+      image_tag("dansyaku.jpg", :size => "20×20", class:"circle")
     when "4"
-      image_tag "jagabata.png", size: "20×20"
+      image_tag("jagabata.png", :size => "20×20", class:"circle")
     end
   end
 end
